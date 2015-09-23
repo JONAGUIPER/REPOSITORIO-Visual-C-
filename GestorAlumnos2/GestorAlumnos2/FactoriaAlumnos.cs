bb16352e55b7cf9e47d25bf9e0471b310369dbb5
jonaguiper <jonaguiper@gmail.com>
2015-09-23 19:04:05 +0200

    Factoria de alumnos

---
+++ GestorAlumnos2/GestorAlumnos2/FactoriaAlumnos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorAlumnos2
+{
+    class FactoriaAlumnos
+    {
+        IList<Alumno> alumnos = new List<Alumno>();
+
+        public FactoriaAlumnos()
+        {
+            cargarDatos();
+        }
+
+        private void cargarDatos()
+        {
+            Alumno al = new Alumno();
+            al.Nombre = "Jon";
+            al.Apellido = "Aguinaco Peraza";
+            alumnos.Add(al);
+        }
+        //MostrarAlumnos
+        public IList<Alumno> MostrarAlumnos()
+        {
+            return alumnos;
+        }
+        //AñadirAlumnos
+        public void AddAlumno(Alumno al)
+        {
+            //alumnos = new List<Alumno>();
+            alumnos.Add(al);
+        }
+    }
+}
--- GestorAlumnos2/GestorAlumnos2/Program.cs
+++ GestorAlumnos2/GestorAlumnos2/Program.cs
@@ -8,14 +8,26 @@ namespace GestorAlumnos2
 {
     class Program
     {
+        static FactoriaAlumnos listaAlumnos = null;
         static void Main(string[] args)
         {
-            EjecutarOpciones(MostrarMenu());
+            listaAlumnos = new FactoriaAlumnos();
+            int opcion = 0;
+            opcion = MostrarMenu();
+            while (opcion < 4 && opcion > 0)
+            {
+                EjecutarOpciones(opcion);
+                opcion=MostrarMenu();
+            }
+            
             Console.ReadKey();
         }
         //Funciones para trabajar
         private static int MostrarMenu()
         {
+            Console.WriteLine("*******************");
+            Console.WriteLine("*      MENU       *");
+            Console.WriteLine("*******************");
             Console.WriteLine("1 - Mostrar Alumnos");
             Console.WriteLine("2 - Añadir Alumnos");
             Console.WriteLine("3 - Salir!!");
@@ -25,16 +37,26 @@ namespace GestorAlumnos2
 
         private static void EjecutarOpciones(int opcion)
         {
+            
             switch (opcion)
             {
                 case 1:
                     {
-                        Console.WriteLine("Mostrar Alumnos");
+                        MostrarAlumnos(listaAlumnos.MostrarAlumnos());
+                        //Console.WriteLine("Mostrar Alumnos");
                         break;
                     }
                 case 2:
                     {
-                        Console.WriteLine("Añadir Alumnos");
+                        Console.WriteLine("******************");
+                        Console.WriteLine("* Añadir Alumnos *");
+                        Console.WriteLine("******************");
+                        Alumno al = new Alumno();
+                        Console.WriteLine("Introduzca Nombre");
+                        al.Nombre=Console.ReadLine();
+                        Console.WriteLine("Introduzca Apellidos");
+                        al.Apellido = Console.ReadLine();
+                        listaAlumnos.AddAlumno(al);
                         break;
                     }
                 case 3:
@@ -47,6 +69,21 @@ namespace GestorAlumnos2
                         Console.WriteLine("Opción Inválida");
                         break;
                     }
+
+            }
+            
+        }
+        private static void MostrarAlumnos(IList <Alumno> alumnos)
+        {
+
+            Alumno al = null;
+            Console.WriteLine("*******************");
+            Console.WriteLine("*  Lista Alumnos  *");
+            Console.WriteLine("*******************");
+            for (int i = 0; i < alumnos.Count; i++)
+            {
+                al=alumnos.ElementAt(i);
+                Console.WriteLine(i+" "+al.Nombre+" "+al.Apellido);
             }
         }
     }
