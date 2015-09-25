ac26ea409f227ef9565c173324c167cccbc4bccd
jonaguiper <jonaguiper@gmail.com>
2015-09-25 17:08:13 +0200

    Holamundo con Form

---
+++ EjemploSolucion1/Ejemplo1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Ejemplo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSaludo = new System.Windows.Forms.Button();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.LblNombre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSaludo
+            // 
+            this.btnSaludo.Location = new System.Drawing.Point(183, 222);
+            this.btnSaludo.Name = "btnSaludo";
+            this.btnSaludo.Size = new System.Drawing.Size(89, 28);
+            this.btnSaludo.TabIndex = 0;
+            this.btnSaludo.Text = "Hola!!!";
+            this.btnSaludo.UseVisualStyleBackColor = true;
+            this.btnSaludo.Click += new System.EventHandler(this.btnSaludo_Click);
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(133, 25);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(139, 20);
+            this.TxtNombre.TabIndex = 1;
+            // 
+            // LblNombre
+            // 
+            this.LblNombre.AutoSize = true;
+            this.LblNombre.Location = new System.Drawing.Point(12, 28);
+            this.LblNombre.Name = "LblNombre";
+            this.LblNombre.Size = new System.Drawing.Size(115, 13);
+            this.LblNombre.TabIndex = 2;
+            this.LblNombre.Text = "Indicar nombre nombre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.LblNombre);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.btnSaludo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSaludo;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.Label LblNombre;
+    }
+}
+
---
+++ EjemploSolucion1/Ejemplo1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludo_Click(object sender, EventArgs e)
+        {
+            string miNombre = string.Empty;
+            
+            if (string.IsNullOrEmpty(TxtNombre.Text))
+            {
+                TxtNombre.BackColor = System.Drawing.Color.Red;
+                MessageBox.Show("Ingrese su Nombre por Favor!!!");
+            }
+            else
+            {
+                miNombre = TxtNombre.Text;
+                MessageBox.Show("Su Nombre es: " + miNombre);
+            }
+
+        }
+
+
+    }
+}
