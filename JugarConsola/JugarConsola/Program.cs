46424c26e6ea948245dc463e20bf01aa103a923c
jonaguiper <jonaguiper@gmail.com>
2015-09-29 17:10:04 +0200

    juegos con consola

---
+++ JugarConsola/JugarConsola/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JugarConsola
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.Beep();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("Toma ya!!!!!!!!!!!!");
+            Console.ReadKey();
+        }
+    }
+}
