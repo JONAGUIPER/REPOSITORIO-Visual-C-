36e8f12154344a3db98b4c35d3ad6434726a26c7
jonaguiper <jonaguiper@gmail.com>
2015-10-08 19:48:44 +0200

    manejo de eventos

---
+++ Ejemplo12/manejoEventos/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace manejoEventos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        RaiseEvent miObjetoDisparaEventos = new RaiseEvent();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //miObjetoDisparaEventos.miEvento += new EventHandler(Pepito);
+            miObjetoDisparaEventos.miEvento += MiObjetoDisparaEventos_miEvento;
+        }
+
+        private void MiObjetoDisparaEventos_miEvento(object sender, EventArgs e)
+        {
+            MessageBox.Show("FORM: disparado");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            miObjetoDisparaEventos.Disparo();
+        }
+    }
+}
---
+++ Ejemplo12/manejoEventos/RaiseEvent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manejoEventos
+{
+    class RaiseEvent
+    {
+        //public delegate void HolaDelegate(string palabra);
+        //public event HolaDelegate miEventoHola = null;
+
+        //declaracion del evento del tipo delegado EventHandler
+        public event EventHandler miEvento = null;
+
+        //metodo donde se dispara el evento
+
+        public void Disparo()
+        {
+            //..
+            System.Windows.Forms.MessageBox.Show("RAISEEVENT: entro en el metodo disparo");
+
+            if (miEvento != null)
+            {
+                System.Windows.Forms.MessageBox.Show("RAISEEVENT: a punto de disparar");
+
+                miEvento(this, new EventArgs());
+            }
+            System.Windows.Forms.MessageBox.Show("RAISEEVENT: sali del metodo disparo");
+        }
+    }
+
+}
