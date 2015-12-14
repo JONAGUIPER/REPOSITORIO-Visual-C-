a31048147468def45ea218383f6ae7b8632e8ec6
jonaguiper <jonaguiper@gmail.com>
2015-12-14 18:23:51 +0100

    favoritos con session

--- MVCFotosAjax/MVCFotosAjax/Controllers/FotoController.cs
+++ MVCFotosAjax/MVCFotosAjax/Controllers/FotoController.cs
@@ -9,11 +9,11 @@ namespace MVCFotosAjax.Controllers
 {
     public class FotoController : Controller
     {
-        
+
         // GET: Foto
         public ActionResult IndiceFotos()
         {
-            return View("Indice",Context.Fotos);
+            return View("Indice", Context.Fotos);
         }
 
         public ActionResult Detalles(int id)
@@ -24,6 +24,39 @@ namespace MVCFotosAjax.Controllers
         public ActionResult Carrousel()
         {
             return View("Carrousel", Context.Fotos);
+        }
+
+        public ActionResult FavoritesSlideShow()
+        {
+            List<Foto> FotosFavoritas = new List<Foto>();
+            List<int> IdsFotosFavoritas = Session["Favoritas"] as List<int>;
+            if (IdsFotosFavoritas == null)
+            {
+                IdsFotosFavoritas = new List<int>();
+            }
+            Foto FotoActual;
+            foreach (int item in IdsFotosFavoritas)
+            {
+                FotoActual = Context.Fotos.Find(f => f.IdFoto == item);
+                if (FotoActual != null)
+                {
+                    FotosFavoritas.Add(FotoActual);
+                }
+
+            }
+            return View("Carrousel", FotosFavoritas);
+        }
+
+        public ActionResult AddFavoritas(int id)
+        {
+            List<int> idsFavoritas = Session["Favoritas"] as List<int>;
+            if (idsFavoritas == null)
+            {
+                idsFavoritas = new List<int>();
+            }
+            idsFavoritas.Add(id);
+            Session["Favoritas"] = idsFavoritas;
+            return Content("LA Foto se ha añadido a sus faroritas", "text/plain",System.Text.Encoding.Default);
         }
     }
 }
--- MVCLoginAutenticacion/MVCLoginAutenticacion/Scripts/MisFunciones.js
+++ MVCLoginAutenticacion/MVCLoginAutenticacion/Scripts/MisFunciones.js
@@ -1,12 +1,12 @@
-﻿$(document).ready()
-{
+﻿$(document).ready(function () {
     $('#DDList').change(function () {
         var i = $('#DDList').val();
-        $.post('@Url.Action("Index", "Orders",new{id=i})').always(function () {
-            $('#target').Load('/Home/UpdatePoints');
-        })
+        //$.post('@Url.Action("Index", "Orders",new{id=i})').always(function () {
+            $("#target").load('@(Url.Action("Index","Orders",null, Request.Url.Scheme))id=' + i);
+        //})
 
     });
-}
+});
+
 
 
